271396bb59a948d6b2acd03da26f4a2aaa19b310
svilenvarbanov2019 <52557863+svilenvarbanov2019@users.noreply.github.com>
2026-07-22 17:01:58 +0300

    Docker support.

--- XRCulture3DReconstruction/build-docker.bat
+++ XRCulture3DReconstruction/build-docker.bat
@@ -3,6 +3,6 @@ echo Publishing...
 dotnet publish XRCulture3DReconstruction/XRCulture3DReconstruction.csproj -c Release -r linux-x64 --self-contained false -o XRCulture3DReconstruction/bin/Release/net8.0/publish
 
 echo Rebuilding and starting Docker container...
-docker compose down
+rem docker compose down
 docker compose build --no-cache
-docker compose up
+rem docker compose up
